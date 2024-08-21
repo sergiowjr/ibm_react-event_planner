54ccfd36e5a78bac95690a7a9558e033eb27820c
Steganza <Stefanbotha1@gmail.com>
2024-08-21 13:05:21 -0400

    Testing gitbranches and added footer to site

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './EventPlanner.css'; // Import CSS file for styling
-
+import Footer from './Footer';
 const EventPlanner = () => {
     return (
         <div className="event-planner-container">
@@ -79,7 +79,7 @@ const EventPlanner = () => {
                     <button className="submit-button">Send</button>
                 </form>
             </section>
-
+            <Footer/>
         </div>
     );
 };
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Footer = () => {
+        return (
+    <>
+    <footer>
+    <p>Event Planner organization</p>
+    <div>
+        Created by Stegan Coding corps
+    </div>
+    </footer>
+    </>
+
+)}
+
+export default Footer
