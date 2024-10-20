2232eda1e6a987cd712df31c6b5d0ee6a5db0c24
Frumosi <kkone.devops@gmail.com>
2024-10-20 04:38:47 -0400

    Updated

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -17,6 +17,33 @@ const EventPlanner = () => {
                 <button className='get-started-button'>Get Started</button>
             </section>
             <section className='events_categories'>
+                <ul>
+                    <h2>Social Events:</h2>
+                    <li>Birthday parties</li>
+                    <li>Anniversary celebrations</li>
+                    <li>Wedding receptions</li>
+                    <li>Baby showers</li>
+                    <li>Graduation parties</li>
+                    <li>Family reunions</li>
+                </ul>
+                <ul>
+                    <h2> Entertainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music festivals</li>
+                    <li>Film screenings</li>
+                    <li>Comedy shows</li>
+                    <li>Art exhibitions</li>
+                    <li>Cultural events</li>
+                </ul>
+                <ul>
+                    <h2>Community Events:</h2>
+                    <li>Fundraising events</li>
+                    <li>Charity galas</li>
+                    <li>Volunteer drives</li>
+                    <li>Neighborhood block parties</li>
+                    <li>Community festivals</li>
+                    <li>Cultural celebrations</li>
+                </ul>
 
             </section>
             <section className='features'>
