27b9f6964dc28f6eb785cac378182de1b5eccacb
Frumosi <kkone.devops@gmail.com>
2024-10-20 04:28:18 -0400

    Updated

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -7,6 +7,27 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+            <section className='description'>
+                <p>
+                   Plan and organize your events effortlessly with Event Planner. From
+                   birthdays to corporate meetings, we've got you covered.
+                </p>
+
+
+                <button className='get-started-button'>Get Started</button>
+            </section>
+            <section className='events_categories'>
+
+            </section>
+            <section className='features'>
+
+            </section>
+            <section className='testimonials'>
+
+            </section>
+            <section className='contact'>
+
+            </section>
         </div>
     );
 };
